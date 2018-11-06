cd5db776e4b9e71929f00840cc52c607727f653f
spacemeowx2 <spacemeowx2@gmail.com>
2018-11-06 20:46:52 +0800

    add ldn_mitm with libstratosphere refactor

---
+++ ldn_mitm/source/ldnmitm_main.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2018 Atmosphère-NX
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <malloc.h>
+
+#include <switch.h>
+#include <stratosphere.hpp>
+
+#include "ldnmitm_service.hpp"
+
+#define TITLE_ID 0x4200000000000010
+
+extern "C" {
+    extern u32 __start__;
+
+    u32 __nx_applet_type = AppletType_None;
+
+    #define INNER_HEAP_SIZE 0x260000
+    size_t nx_inner_heap_size = INNER_HEAP_SIZE;
+    char   nx_inner_heap[INNER_HEAP_SIZE];
+    
+    void __libnx_initheap(void);
+    void __appInit(void);
+    void __appExit(void);
+}
+
+
+void __libnx_initheap(void) {
+	void*  addr = nx_inner_heap;
+	size_t size = nx_inner_heap_size;
+
+	/* Newlib */
+	extern char* fake_heap_start;
+	extern char* fake_heap_end;
+
+	fake_heap_start = (char*)addr;
+	fake_heap_end   = (char*)addr + size;
+}
+
+void registerFspLr()
+{
+    if (kernelAbove400())
+        return;
+
+    Result rc = fsprInitialize();
+    if (R_FAILED(rc))
+        fatalLater(rc);
+
+    u64 pid;
+    svcGetProcessId(&pid, CUR_PROCESS_HANDLE);
+
+    rc = fsprRegisterProgram(pid, TITLE_ID, FsStorageId_NandSystem, NULL, 0, NULL, 0);
+    if (R_FAILED(rc))
+        fatalLater(rc);
+    fsprExit();
+}
+
+void __appInit(void) {
+    Result rc;
+    svcSleepThread(10000000000L);
+    
+    rc = smInitialize();
+    if (R_FAILED(rc)) {
+        fatalLater(rc);
+    }
+    
+    rc = fsInitialize();
+    if (R_FAILED(rc)) {
+        fatalLater(rc);
+    }
+    registerFspLr();
+    
+    rc = fsdevMountSdmc();
+    if (R_FAILED(rc)) {
+        fatalLater(rc);
+    }
+    CheckAtmosphereVersion();
+    LogStr("__appInit done\n");
+}
+
+void __appExit(void) {
+    /* Cleanup services. */
+    fsdevUnmountAll();
+    fsExit();
+    smExit();
+}
+
+struct LdnMitmManagerOptions {
+    static const size_t PointerBufferSize = 0x800;
+    static const size_t MaxDomains = 0x10;
+    static const size_t MaxDomainObjects = 0x4000;
+};
+using LdnMitmManager = WaitableManager<LdnMitmManagerOptions>;
+
+int main(int argc, char **argv)
+{
+    consoleDebugInit(debugDevice_SVC);
+    LogStr("main\n");
+
+    /* TODO: What's a good timeout value to use here? */
+    auto server_manager = new LdnMitmManager(5);
+
+    /* Create ldn:s mitm. */
+    AddMitmServerToManager<LdnMitMService>(server_manager, "ldn:u", 61);
+
+    LogStr("main process\n");
+    /* Loop forever, servicing our services. */
+    server_manager->Process();
+    LogStr("main process done\n");
+
+    return 0;
+}
---
+++ ldn_mitm/source/ldnmitm_service.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <switch.h>
+#include <stratosphere.hpp>
+#include "ldn_icommunication.hpp"
+#include "debug.hpp"
+
+enum LdnSrvCmd {
+    LdnSrvCmd_CreateUserLocalCommunicationService = 0,
+};
+
+class LdnMitMService : public IMitmServiceObject {      
+    private:
+        // std::shared_ptr<IStorageInterface> romfs_storage;
+    public:
+        LdnMitMService(std::shared_ptr<Service> s) : IMitmServiceObject(s) {
+            LogStr("LdnMitMService\n");
+            /* ... */
+        }
+        
+        static bool ShouldMitm(u64 pid, u64 tid) {
+            char buf[128];
+            sprintf(buf, "should_mitm pid: %" PRIu64 " tid: %" PRIu64 "\n", pid, tid);
+            LogStr(buf);
+            return true;
+        }
+        
+        static void PostProcess(IMitmServiceObject *obj, IpcResponseContext *ctx) {};
+    protected:
+        /* Overridden commands. */
+        Result CreateUserLocalCommunicationService(Out<std::shared_ptr<ICommunicationInterface>> out);
+    public:
+        DEFINE_SERVICE_DISPATCH_TABLE {
+            MakeServiceCommandMeta<LdnSrvCmd_CreateUserLocalCommunicationService, &LdnMitMService::CreateUserLocalCommunicationService>(),
+        };
+};
